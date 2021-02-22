33ce5f8e3a7a5585ec455fdbdb83a36c0f404189
Nathan Chin <nathangoffriller@berkeley.edu>
2021-02-21 18:23:32 -0800

    graphql start

---
+++ my-react-blog/graphql/schemas/posts/mocks/featured.js
@@ -0,0 +1,34 @@
+const moment = require('moment')
+
+module.exports = [
+    {
+        title: 'can anyone code?',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture', 'Tech News'],
+        link: '#',
+        image: 'anyone_can_code.jpeg',
+    },
+
+    {
+        title: 'Using AWS S3 for Storing Blog Images',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud'],
+        link: '#',
+        image: 'cloud-storage.jpg'
+    },
+    {
+        title: 'can anyone code?',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture', 'Tech News'],
+        link: '#',
+        image: 'anyone_can_code.jpeg',
+    },
+
+    {
+        title: 'Using AWS S3 for Storing Blog Images',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud'],
+        link: '#',
+        image: 'cloud-storage.jpg'
+    }
+]
---
+++ my-react-blog/graphql/schemas/posts/mocks/trending.js
@@ -0,0 +1,83 @@
+const moment = require('moment')
+
+module.exports = [
+    {
+        title: 'How to Make Olive Bread',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud', 'Bread'],
+        link: '#',
+        image: 'olive-bread.jpeg',
+    },
+
+    {
+        title: 'Cold for 48 Hours',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Hydroflask'],
+        link: '#',
+        image: 'hydro.jpeg',
+    },
+
+    {
+        title: 'How to Make Olive Bread',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud', 'Bread'],
+        link: '#',
+        image: 'olive-bread.jpeg',
+    },
+
+    {
+        title: 'Cold for 48 Hours',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Hydroflask'],
+        link: '#',
+        image: 'hydro.jpeg',
+    },
+
+    {
+        title: 'How to Make Olive Bread',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud', 'Bread'],
+        link: '#',
+        image: 'olive-bread.jpeg',
+    },
+
+    {
+        title: 'Cold for 48 Hours',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Hydroflask'],
+        link: '#',
+        image: 'hydro.jpeg',
+    },
+
+    {
+        title: 'How to Make Olive Bread',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud', 'Bread'],
+        link: '#',
+        image: 'olive-bread.jpeg',
+    },
+
+    {
+        title: 'Cold for 48 Hours',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Hydroflask'],
+        link: '#',
+        image: 'hydro.jpeg',
+    },
+
+    {
+        title: 'How to Make Olive Bread',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud', 'Bread'],
+        link: '#',
+        image: 'olive-bread.jpeg',
+    },
+
+    {
+        title: 'Cold for 48 Hours',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Hydroflask'],
+        link: '#',
+        image: 'hydro.jpeg',
+    }
+]
